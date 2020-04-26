c61acd0aa0a9e6de2abd76ae835020faee865378
urain39 <urain39@qq.com>
2020-04-26 20:36:00 +0800

    feat: dump save file with game name(from title)

--- js/rpg_hooks.js
+++ js/rpg_hooks.js
@@ -1,4 +1,4 @@
-﻿// Author: urain39
+// Author: urain39
 // FIXED: Mobile Devices Such As Android Cannot Run At Browser
 
 /**** BEGIN OF THE HOOKS ****/
@@ -9,10 +9,12 @@ if (!WebAudio.canPlayOgg()) {
     SceneManager.stop();
     AudioManager.stopAll();
     // NOTE: Canvas doesn't initiated now.
-    alert("Doesn't support on your browser!");
+    alert('Doesn\'t support on your browser!');
 }
 
-$(window).on("load", function(event) {
+var $saveFile = 'rpg-saves(' + $('title').text() + ').json';
+
+$(window).on('load', function(event) {
     /*
      * For Debug.
      */
@@ -20,46 +22,46 @@ $(window).on("load", function(event) {
     var $vConsolse = new VConsole();
 
     $(document)
-        .on("keydown", function(event) {
-            console.log("Key Down: " + event.key);
+        .on('keydown', function(event) {
+            console.log('Key Down: ' + event.key);
         })
-        .on("keyup", function(event) {
-            console.log("Key Up: " + event.key);
+        .on('keyup', function(event) {
+            console.log('Key Up: ' + event.key);
         });
 
     /*
      * Functions Binding.
      */
-    $("#ctrl-key-esc").on("click", function(event) {
-        Hooks.fireKey("Escape"); return false;
+    $('#ctrl-key-esc').on('click', function(event) {
+        Hooks.fireKey('Escape'); return false;
     });
-    $("#ctrl-key-ok").on("click", function(event) {
-        Hooks.fireKey("Enter"); return false;
+    $('#ctrl-key-ok').on('click', function(event) {
+        Hooks.fireKey('Enter'); return false;
     });
-    $("#ctrl-key-f2").on("click", function(event) {
-        Hooks.fireKey("F2"); return false;
+    $('#ctrl-key-f2').on('click', function(event) {
+        Hooks.fireKey('F2'); return false;
     });
-    $("#ctrl-key-f4").on("click", function(event) {
-        Hooks.fireKey("F4"); return false;
+    $('#ctrl-key-f4').on('click', function(event) {
+        Hooks.fireKey('F4'); return false;
     });
-    $("#action-quick-load").on("click", function(event) {
+    $('#action-quick-load').on('click', function(event) {
         Hooks.quickLoad(); return false;
     });
-    $("#action-quick-save").on("click", function(event) {
+    $('#action-quick-save').on('click', function(event) {
         Hooks.quickSave(); return false;
     });
-    $("#action-read-saves").on("click", function(event) {
+    $('#action-read-saves').on('click', function(event) {
         Hooks.readSaves(); return false;
     });
-    $("#action-dump-saves").on("click", function(event) {
+    $('#action-dump-saves').on('click', function(event) {
         Hooks.dumpSaves(); return false;
     });
 
     /*
      * Fix click on Mobile Devices.
      */
-    $(".control-key").each(function (idx, elem) {
-        $(elem).on("touchstart", function(event) {
+    $('.control-key').each(function (idx, elem) {
+        $(elem).on('touchstart', function(event) {
             elem.click(); return false;
         });
     });
@@ -69,52 +71,52 @@ $(window).on("load", function(event) {
     */
     function _$(selector, keyName) {
         $(selector)
-            .on("touchstart", function(event) {
+            .on('touchstart', function(event) {
                 Hooks.fireKeyDown(keyName); return false;
             })
-            .on("touchend", function(event) {
+            .on('touchend', function(event) {
                 Hooks.fireKeyUp(keyName); return false;
             });
     }
    /*
     * Mapings Binding.
     */
-    _$("#ctrl-key-left", "ArrowLeft");
-    _$("#ctrl-key-right", "ArrowRight");
-    _$("#ctrl-key-up", "ArrowUp");
-    _$("#ctrl-key-down", "ArrowDown");
+    _$('#ctrl-key-left', 'ArrowLeft');
+    _$('#ctrl-key-right', 'ArrowRight');
+    _$('#ctrl-key-up', 'ArrowUp');
+    _$('#ctrl-key-down', 'ArrowDown');
 });
 
 AudioManager.audioFileExt = function() {
-    return ".ogg"; // Only checking once.
+    return '.ogg'; // Only checking once.
 };
 
 Hooks = function() {
-    throw new Error("This is a static class!");
+    throw new Error('This is a static class!');
 };
 
 Hooks.keyNameMapper = {
-    "ArrowLeft" : 37,
-    "ArrowUp"   : 38,
-    "ArrowRight": 39,
-    "ArrowDown" : 40,
-    "PageDown"  : 33,
-    "PageUp"    : 34,
-    "Escape"    : 27,
-    "Enter"     : 13,
-    "F2"        : 113, // Show FPSMeter
-    "F4"        : 115, // Switch FullScreen
+    'ArrowLeft' : 37,
+    'ArrowUp'   : 38,
+    'ArrowRight': 39,
+    'ArrowDown' : 40,
+    'PageDown'  : 33,
+    'PageUp'    : 34,
+    'Escape'    : 27,
+    'Enter'     : 13,
+    'F2'        : 113, // Show FPSMeter
+    'F4'        : 115, // Switch FullScreen
 };
 
 Hooks.fireKeyDown = function(keyName) {
-    document.dispatchEvent(new KeyboardEvent("keydown", {
+    document.dispatchEvent(new KeyboardEvent('keydown', {
         key: keyName,
         keyCode: this.keyNameMapper[keyName],
     }));
 };
 
 Hooks.fireKeyUp = function(keyName) {
-    document.dispatchEvent(new KeyboardEvent("keyup", {
+    document.dispatchEvent(new KeyboardEvent('keyup', {
         key: keyName,
         keyCode: this.keyNameMapper[keyName],
     }));
@@ -146,9 +148,9 @@ Hooks.readSaves = function() {
     function fetchFile() {
         return new Promise(function (resolve, reject) {
             var xhr = new XMLHttpRequest();
-            xhr.open("GET", "rpg-saves.json", true);
-            xhr.responseType = "json";
-            xhr.setRequestHeader("Content-Type", "text/plain");
+            xhr.open('GET', $saveFile, true);
+            xhr.responseType = 'json';
+            xhr.setRequestHeader('Content-Type', 'text/plain');
             xhr.onload = function() {
                 if (this.status === 200) {
                     resolve(this.response);
@@ -159,35 +161,35 @@ Hooks.readSaves = function() {
         });
     }
 
-    alert("WARNING: This operation will overwrite your current saves!");
+    alert('WARNING: This operation will overwrite your current saves!');
 
-    if (confirm("Are you sure you want to overwrite?")) {
+    if (confirm('Are you sure you want to overwrite?')) {
         fetchFile()
             .then(function(data) {
                 var k,
-                    buf = data;
+                    temp = data;
                 // Apply saves.
-                for (k in buf) {
-                    localStorage.setItem(k, buf[k]);
+                for (k in temp) {
+                    localStorage.setItem(k, temp[k]);
                 }
             })
             .then(function(data) {
-                alert("Overwrited.");
+                alert('Overwrited.');
             });
 
         return;
     }
 
-    alert("Canceled.");
+    alert('Canceled.');
 };
 
 Hooks.dumpSaves = function() {
-    function dumpFile(filename, text) {
-        var elem = document.createElement("a");
-        elem.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(text));
-        elem.setAttribute("download", filename);
+    function dumpSaveFile(filename, text) {
+        var elem = document.createElement('a');
+        elem.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+        elem.setAttribute('download', filename);
 
-        elem.style.display = "none";
+        elem.style.display = 'none';
         document.body.appendChild(elem);
 
         elem.click();
@@ -195,14 +197,14 @@ Hooks.dumpSaves = function() {
     }
 
     var i, k,
-        buf = {};
+        temp = {};
 
     for (i = 0; i < localStorage.length; i++) {
         k = localStorage.key(i);
-        buf[k] = localStorage.getItem(k);
+        temp[k] = localStorage.getItem(k);
     }
 
-    dumpFile("rpg-saves.json", JSON.stringify(buf));
+    dumpSaveFile($saveFile, JSON.stringify(temp));
 };
 
 /**** END OF THE HOOKS ****/
